abf0296b91f205ae709b79893540dd7bed4d2f88
Sv812asx <122466233+Sv812asx@users.noreply.github.com>
2023-08-11 14:23:13 +0300

    Осталось 649 ошибок

--- src/component/ticket-segment/ticket-segment.js
+++ src/component/ticket-segment/ticket-segment.js
@@ -1,35 +1,46 @@
-import styles from "../ticket-list-item/ticket-list-item.module.scss";
-import {addDurationToDate} from "../../Utilities/addDurationToDate";
-import {formatTime} from "../../Utilities/formatTime";
-import {transformStops} from "../../Utilities/transformStops";
+import styles from '../ticket-list-item/ticket-list-item.module.scss';
+import addDurationToDate from '../../Utilities/addDurationToDate';
+import { formatTime } from '../../Utilities/formatTime';
+import { transformStops } from '../../Utilities/transformStops';
 
 function TicketSegment({ segment }) {
-    const { ticket__data, ticket__dataRoute, ticket__allocated, ticket__dataEnRoute, ticket__dataTransplants } = styles;
-    const hours = Math.floor(segment.duration / 60);
-    const minutes = segment.duration % 60;
-    const stops = segment.stops.join(', ');
-    const stopsCount = segment.stops.length;
+  const { ticketData, ticketDataRoute, ticketAllocated, ticketDataEnRoute, ticketDtaTransplants } = styles;
+  const hours = Math.floor(segment.duration / 60);
+  const minutes = segment.duration % 60;
+  const stops = segment.stops.join(', ');
+  const stopsCount = segment.stops.length;
 
-    const arrivalTime = addDurationToDate(segment.date, segment.duration);
-    return (
-        <div className={ticket__data}>
-            <div className={ticket__dataRoute}>
-                <span className={ticket__allocated}>{segment.origin} - {segment.destination}</span>
-                <span>{formatTime(segment.date)} - {arrivalTime} </span>
-            </div>
-            <div className={ticket__dataEnRoute}>
-                <span className={ticket__allocated}>В пути</span>
-                <span>{hours} ч {minutes} м</span>
-            </div>
-            <div className={ticket__dataTransplants}>
-                {stopsCount > 0 ?
-                    <span className={ticket__allocated}>{stopsCount} {transformStops(stopsCount)}</span> :
-                    <span className={ticket__allocated}>Без пересадок</span>
-                }
-                {stopsCount > 0 && <span>{stops}</span>}
-            </div>
-        </div>
-    );
+  const arrivalTime = addDurationToDate(segment.date, segment.duration);
+  return (
+    <div className={ticketData}>
+      <div className={ticketDataRoute}>
+        <span className={ticketAllocated}>
+          {segment.origin} - {segment.destination}
+        </span>
+        <span>
+          {formatTime(segment.date)} - {arrivalTime}
+        </span>
+      </div>
+      <div className={ticketDataEnRoute}>
+        <span className={ticketAllocated}>В пути</span>
+        <span>
+          {hours} ч {minutes} м
+        </span>
+      </div>
+      <div className={ticketDtaTransplants}>
+        {stopsCount > 0 ? (
+          <>
+            <span className={ticketAllocated}>
+              {stopsCount} {transformStops(stopsCount)}
+            </span>
+            <span>{stops}</span>
+          </>
+        ) : (
+          <span className={ticketAllocated}>Без пересадок</span>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default TicketSegment;
--- src/redux/types.js
+++ src/redux/types.js
@@ -1,18 +1,12 @@
 export const ALL_CHECKED = 'ALL_CHECKED';
 export const UN_CHECKED_ALL = 'UN_CHECKED_ALL';
 export const TOGGLE_CHECK = 'TOGGLE_CHECK';
-
-export const GET_SEARCH_ID = 'GET_SEARCH_ID'
+export const GET_SEARCH_ID = 'GET_SEARCH_ID';
 export const TICKETS_LOAD = 'TICKETS_LOAD';
-
 export const FILTER_TICKET = 'FILTER_TICKET';
-
 export const START_LOADING = 'START_LOADING';
 export const STOP_LOADING = 'STOP_LOADING';
-
 export const NO_TICKETS_FOUND = 'NO_TICKETS_FOUND';
-
 export const CHEAPEST_TICKETS = 'CHEAPEST_TICKETS';
 export const FASTEST_TICKETS = 'FASTEST_TICKETS';
-
 export const FIVE_TICKETS = 'FIVE_TICKETS';

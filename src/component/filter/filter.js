177c8493d6368feb08d201f101ef88c802418c28
Sv812asx <122466233+Sv812asx@users.noreply.github.com>
2023-08-11 14:43:36 +0300

    Осталось 470 ошибок

--- src/component/filter/filter.js
+++ src/component/filter/filter.js
@@ -1,69 +1,65 @@
-import styles from "./filter.module.scss";
-import {useDispatch, useSelector} from "react-redux";
-import {filterAllUnchecked, filterAllChecked, toggleCheck, filterTickets} from "../../redux/actions";
+import { useDispatch, useSelector } from 'react-redux';
+
+import { filterAllUnchecked, filterAllChecked, toggleCheck, filterTickets } from '../../redux/actions';
+
+import styles from './filter.module.scss';
 
 function Filter() {
-    const title = 'Количество пересадок'
-    const dispatch = useDispatch();
-    const {allChecked, oneChecked, twoChecked, threeChecked, fourChecked} = useSelector(state => state.filter);
-    const handleAllCheckBoxChange = (e) => {
-        if (e.target.checked) {
-            dispatch(filterAllChecked());
-            dispatch(filterTickets());
-        } else {
-            dispatch(filterAllUnchecked());
-        }
+  const title = 'Количество пересадок';
+  const dispatch = useDispatch();
+  const { allChecked, oneChecked, twoChecked, threeChecked, fourChecked } = useSelector((state) => state.filter);
+  const handleAllCheckBoxChange = (e) => {
+    if (e.target.checked) {
+      dispatch(filterAllChecked());
+      dispatch(filterTickets());
+    } else {
+      dispatch(filterAllUnchecked());
     }
+  };
 
-    const handleCheckBoxChange = (name) => {
-        dispatch(toggleCheck(name));
-        dispatch(filterTickets());
-    }
+  const handleCheckBoxChange = (name) => {
+    dispatch(toggleCheck(name));
+    dispatch(filterTickets());
+  };
 
-    const {
-        aviasales__filter,
-        aviasales__filterTitle,
-        aviasales__filterHover,
-        checkboxContainer,
-        checkboxContainer__text
-    } = styles
+  const { aviasalesFilter, aviasalesFilterTitle, aviasalesFilterHover, checkboxContainer, checkboxContainerText } =
+    styles;
 
-    return (
-        <section className={aviasales__filter}>
-            <h1 className={aviasales__filterTitle}>{title.toUpperCase()}</h1>
-            <div className={aviasales__filterHover}>
-                <label className={checkboxContainer}>
-                    <input type="checkbox" checked={allChecked} onChange={handleAllCheckBoxChange}/>
-                    <span className={checkboxContainer__text}>Все</span>
-                </label>
-            </div>
-            <div className={aviasales__filterHover}>
-                <label className={checkboxContainer}>
-                    <input type="checkbox" checked={oneChecked} onChange={() => handleCheckBoxChange('oneChecked')}/>
-                    <span className={checkboxContainer__text}>Без пересадок</span>
-                </label>
-            </div>
-            <div className={aviasales__filterHover}>
-                <label className={checkboxContainer}>
-                    <input type="checkbox" checked={twoChecked} onChange={() => handleCheckBoxChange('twoChecked')}/>
-                    <span className={checkboxContainer__text}>1 пересадка</span>
-                </label>
-            </div>
-            <div className={aviasales__filterHover}>
-                <label className={checkboxContainer}>
-                    <input type="checkbox" checked={threeChecked}
-                           onChange={() => handleCheckBoxChange('threeChecked')}/>
-                    <span className={checkboxContainer__text}>2 пересадки</span>
-                </label>
-            </div>
-            <div className={aviasales__filterHover}>
-                <label className={checkboxContainer}>
-                    <input type="checkbox" checked={fourChecked} onChange={() => handleCheckBoxChange('fourChecked')}/>
-                    <span className={checkboxContainer__text}>3 пересадки</span>
-                </label>
-            </div>
-        </section>
-    )
+  return (
+    <section className={aviasalesFilter}>
+      <h1 className={aviasalesFilterTitle}>{title.toUpperCase()}</h1>
+      <div className={aviasalesFilterHover}>
+        <label className={checkboxContainer}>
+          <input type="checkbox" checked={allChecked} onChange={handleAllCheckBoxChange} />
+          <span className={checkboxContainerText}>Все</span>
+        </label>
+      </div>
+      <div className={aviasalesFilterHover}>
+        <label className={checkboxContainer}>
+          <input type="checkbox" checked={oneChecked} onChange={() => handleCheckBoxChange('oneChecked')} />
+          <span className={checkboxContainerText}>Без пересадок</span>
+        </label>
+      </div>
+      <div className={aviasalesFilterHover}>
+        <label className={checkboxContainer}>
+          <input type="checkbox" checked={twoChecked} onChange={() => handleCheckBoxChange('twoChecked')} />
+          <span className={checkboxContainerText}>1 пересадка</span>
+        </label>
+      </div>
+      <div className={aviasalesFilterHover}>
+        <label className={checkboxContainer}>
+          <input type="checkbox" checked={threeChecked} onChange={() => handleCheckBoxChange('threeChecked')} />
+          <span className={checkboxContainerText}>2 пересадки</span>
+        </label>
+      </div>
+      <div className={aviasalesFilterHover}>
+        <label className={checkboxContainer}>
+          <input type="checkbox" checked={fourChecked} onChange={() => handleCheckBoxChange('fourChecked')} />
+          <span className={checkboxContainerText}>3 пересадки</span>
+        </label>
+      </div>
+    </section>
+  );
 }
 
 export default Filter;
--- src/component/loader/loader.js
+++ src/component/loader/loader.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Spin } from 'antd';
+
 export default function Loader() {
-    const isLoading = useSelector(state => state.ticket.loading);
+  const isLoading = useSelector((state) => state.ticket.loading);
 
-    if (!isLoading) return null;
+  if (!isLoading) return null;
 
-    return (
-            <Spin size="large" />
-    );
+  return <Spin size="large" />;
 }
